4d509862223ba4bc5b77224ca0604a6e7ac0b783
hirodinn <hirebikila1@gmail.com>
2026-01-20 21:19:44 +0300

    feat: Implement hash-based scrolling and move ScrollToTop component to the utils directory.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ScrollToTop from "./ScrollToTop.jsx";
+import ScrollToTop from "./utils/ScrollToTop.jsx";
+import ScrollToHash from "./utils/ScrollToHash.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ScrollToTop />
+      <ScrollToHash />
       <App />
     </BrowserRouter>
   </StrictMode>
